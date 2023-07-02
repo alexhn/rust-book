a4f7e284725ec0601e7de63157cc781c22b2d8f8
Aleksandr <alex@alexn.ee>
2023-07-02 02:09:57 -0400

    Variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -16,4 +16,10 @@ fn main() {
     }
 
     println!("The value of y is: {y}");
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
 }
