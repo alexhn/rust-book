4c10dfa020373a26037b3fdb8c6b3ffdae103f35
Aleksandr <alex@alexn.ee>
2023-07-02 02:12:42 -0400

    Variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -22,4 +22,17 @@ fn main() {
     let (x, y, z) = tup;
 
     println!("The value of y is: {y}");
+
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+
+    let one = tup.2;
+
+    let a = [1, 2, 3, 4, 5];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    
 }
